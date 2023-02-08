17669797d58623ae132194cf31c4f8a04aab6f30
birenyadav <afraz>
2023-02-08 23:19:11 +0530

    sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
+
+
+
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
@@ -16,16 +15,13 @@ import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SidebarComponent,
-    FooterComponent,
-    HeaderComponent
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+
     HttpClientModule,
-    AppRoutingModule,
+ 
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
@@ -33,6 +29,6 @@ import {MatButtonModule} from '@angular/material/button';
     MatIconModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+
 })
 export class AppModule { }
